22c12a6257fbe5e250d506ebcea2e26453ba2d9d
mateus_winicius <mateus_winicius@SNSOO1EP01VM149.saojose.sc.senai.br>
2015-11-17 09:48:08 -0200

    Criação do template.

--- template/header.php
+++ template/header.php
@@ -1,8 +1,7 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title><?php echo $title; ?></title>
+        <link type="text/css" href="template/style/app.css"
+    </head>
+    <body>
